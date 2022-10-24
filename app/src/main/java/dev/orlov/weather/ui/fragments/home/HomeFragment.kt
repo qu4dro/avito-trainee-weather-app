1e078d060d0ba3d73efc1b43dabccccc61d40df0
Orlov Peter <qu4dro.sl@gmail.com>
2022-10-24 12:51:33 +0800

    Error toast

--- app/src/main/java/dev/orlov/weather/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/dev/orlov/weather/ui/fragments/home/HomeFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Lifecycle
@@ -113,6 +114,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private fun setErrorUi() {
         binding.apply {
             swipeRefresh.isRefreshing = false
+            Toast.makeText(requireContext(), getText(R.string.something_went_wrong), Toast.LENGTH_SHORT).show()
         }
     }
 
